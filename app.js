60d56fa9d70f29813a102c78845fb2f564848f45
Idan <idanbendavid10@hotmail.com>
2022-02-28 13:39:50 +0200

    remove * in line 23

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ server.listen(process.env.PORT || 8080);
 
 server.use(express.static('./dist/project'));
 
-server.get('/*', (req, res) =>
-    res.sendFile(path.join('index.html', { root: 'dist/project/' })),
+server.get('/', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/project/' }),
     console.log("all working")
 );
 
